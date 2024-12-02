9ef854ae80b1b709adbcdb24489860c9f79288d4
Дмитрий <overlord381@gmail.com>
2024-12-02 22:48:28 +0200

    update

---
+++ Module_3_2.py
@@ -0,0 +1,25 @@
+import re
+
+regex = re.compile(r'([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[ru, com, net]{2,})+')
+
+def send_email(message, recipient, sender = "university.help@gmail.com"):
+    if not re.fullmatch(regex,sender) or not re.fullmatch(regex,recipient):
+        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+
+    elif recipient == sender:
+        print("Нельзя отправить письмо самому себе!")
+
+    elif sender == "university.help@gmail.com":
+        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+
+    elif sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
